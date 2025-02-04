a2659f09acb1025bde91bbdd3115c7cda8c2ca04
Inanc <inancarin@sabanciuniv.edu>
2025-02-04 22:54:55 +0300

    week0

--- Week00/Lecture/first_file.py
+++ Week00/Lecture/first_file.py
@@ -5,9 +5,18 @@ def someFunc(a, b):
     b += 100
     print("End of the function")
 
+def anotherFunc(s):
+    if len(s) > 5:
+        check = True
+    if check:
+        print("The length of the string is greater than 5")
+    else:
+        print("The length of the string is not greater than 5")
+
 
 # main part
 x = 10
 y = 20
 someFunc(x, y)
+anotherFunc("dsa201")
 print("End of the program")
