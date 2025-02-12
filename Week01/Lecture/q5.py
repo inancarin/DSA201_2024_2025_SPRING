834b83c15df684e15600cee454bbb90d78bd4a8d
Inanc Arin <inancarin@sabanciuniv.edu>
2025-02-12 16:36:04 +0300

    week01

--- Week01/Lecture/q5.py
+++ Week01/Lecture/q5.py
@@ -1,23 +1,44 @@
 import math
 
-s = "if man was meant to stay on the ground god would have given us roots"
-s = s.replace(" ", "")
-L = len(s)
-print(L)
+def encrypt(s):
+    s = s.replace(" ", "")
+    L = len(s)
+    print(L)
 
-val = math.sqrt(L)
-row = int(val)
-col = row
-if row < val:
-    col += 1
+    val = math.sqrt(L)
+    row = int(val)
+    col = row
+    if row < val:
+        col += 1
 
-print(row, col)
-print(s)
-"""
-mat = [
-    ["i", "f", "m", "a", "n", "w", "a", "s"],
-    ["m", "e", "a", "n", "t", "t", "o", "s"],
-    ...
-    ["s", "r", "o", "o", "t", "s", "", ""]
-]
-"""
+    mat = []
+    for i in range(row):
+        temp = []
+        for j in range(col):
+            if i * col + j < L:
+                temp.append(s[i * col + j])
+            else:
+                temp.append("")
+        mat.append(temp)
+
+    """
+    mat = [
+        ["i", "f", "m", "a", "n", "w", "a", "s"],
+        ["m", "e", "a", "n", "t", "t", "o", "s"],
+        ...
+        ["s", "r", "o", "o", "t", "s", "", ""]
+    ]
+    """
+
+    res = ""
+    for j in range(col):
+        for i in range(row):
+            res += mat[i][j]
+        res += " "
+    
+    return res
+
+# main
+
+res = encrypt("have a nice day")
+print(res)
